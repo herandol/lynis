79b7905823fdf414fb18ee02c5f91fa8a572f284
herandol <herandol@pm.me>
2021-10-09 17:28:26 +0200

    auto

--- scripts/configure_package_management.sh
+++ scripts/configure_package_management.sh
@@ -3,5 +3,4 @@
 apt-get install -y debsums
 apt-get install -y apt-show-versions
 apt-get install -y unattended-upgrades
-mv /etc/apt/apt.conf.d/20auto-upgrades /etc/apt/apt.conf.d/20auto-upgrades.old
 cp 20auto-upgrades /etc/apt/apt.conf.d/20auto-upgrades
