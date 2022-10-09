a91093c679abe2ecbb8314b1c190bbc63cc90c83
herandol <herandol@pm.me>
2022-10-09 16:52:41 +0200

    configure unattended-upgrades

--- scripts/configure_package_management.sh
+++ scripts/configure_package_management.sh
@@ -4,3 +4,7 @@ apt-get install -y debsums
 apt-get install -y apt-show-versions
 apt-get install -y unattended-upgrades
 cp 20auto-upgrades /etc/apt/apt.conf.d/20auto-upgrades
+file_unattended_upgrades="/etc/apt/apt.conf.d/50unattended-upgrades"
+current_time=$(date +%k%M%S)
+sed -i.backup."$current_time" '/\/\/      "${distro_id}:${distro_codename}-updates";/ c\\        "${distro_id}:${distro_codename}-updates";' "$file_unattended_upgrades"
+unattended-upgrades --dry-run
